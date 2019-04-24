375f34120ea09af9f6f5c199113b4a9291185ebf
Sanjay Sai <shachsan@hotmail.com>
2019-04-23 20:39:53 -0400

    style draw card

--- src/containers/DrawnCardContainer.js
+++ src/containers/DrawnCardContainer.js
@@ -3,22 +3,17 @@ import React, { Component } from 'react';
 export default class DrawnCardContainer extends Component {
 
 
-    getSuit=(suit)=>{
-        if(suit==='spades') return "spades";
-        if(suit==='club') return "club";
-        if(suit==='diamond') return "diamond";
-        if(suit==='heart') return "heart";
-    }
-
     showDrawCards=(cards)=>{
         console.log('drawn cards in DrawnCardContainer', cards);
         const cardImgs=[];
         cards.map(card=>{
             return cardImgs.push(
                 <div className="draw-card" key={card.value+card.suit}>
-                    <div className="">{card.value}</div>
+                    <div className={`card-top-value card-value-${card.suit}`}>{card.value}</div>
+                    <div className={`top-${card.suit}`}></div>
                     <div className={card.suit}></div>
-                    <div className="">{card.value}</div>
+                    <div className={`bottom-${card.suit}`}></div>
+                    <div className={`card-bottom-value card-value-${card.suit}`}>{card.value}</div>
                     
                 </div>)
         })
